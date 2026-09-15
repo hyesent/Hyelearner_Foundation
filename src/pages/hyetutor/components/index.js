9c76fe7fb97572c674e460833aaec7a9abe1d754
hyesent <hyacinthmichael36@gmail.com>
2026-09-15 15:58:52 +0100

    Update index.js

--- src/pages/hyetutor/components/index.js
+++ src/pages/hyetutor/components/index.js
@@ -6,7 +6,7 @@
 export { MissionHero } from './MissionHero'
 export { PerformanceCenter } from './PerformanceCenter'
 export { WeeklyMomentum } from './WeeklyMomentum'
-export { HyeTutorChat } from './HyeTutorChat'
+export { default as HyeTutorChatPage } from './HyeTutorChatPage'
 export { AIHabits } from './AIHabits'
 export { QuickActions } from './QuickActions'
 export { ReflectionModal } from './ReflectionModal'
